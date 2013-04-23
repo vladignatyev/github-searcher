e4aba0b5288d5796f351c98afb0db8894b6e051d
Vladimir Ignatyev <vignatyev@flexis.ru>
2013-04-23 13:49:54 +0400

    improved usage, added CSV exporting features

--- retrieve.py
+++ retrieve.py
@@ -1,38 +1,51 @@
+#!env/bin/python
+
 import lib
+import sys
+import unicodecsv as csv
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option("-f", "--file", dest="filename",
+                  help="write report to FILE instead of presenting it on screen", metavar="FILE")
+(options, args) = parser.parse_args()
+
+f = None
+if options.filename:
+    f = open(options.filename, 'wb')
+    print "Writing report to file: %s" % options.filename
+    print "---"
 
 keywords = raw_input('Enter keywords, divided with comma:').split(',')
 language = raw_input('Enter language [Objective-C]:')
+
 if not language:
     language = lib.OBJECTIVE_C
 
-# {u'fork': False, 
-# u'has_wiki': True, 
-# u'private': False, 
-# u'pushed': u'2012-02-28T15:44:29Z', 
-# u'owner': u'gimenete', 
-# u'size': 168, 
-# u'score': 118.713745, 
-# u'followers': 2418, 
-# u'forks': 374, 
-# u'homepage': u'http://iosboilerplate.com/', 
-# u'username': u'gimenete', 
-# u'description': u'iOS-app template with lots of common tasks solved', 
-# u'has_downloads': True, 
-# u'has_issues': True, 
-# u'watchers': 2418, 
-# u'name': u'iOS-boilerplate', 
-# u'language': u'Objective-C', 
-# u'created': u'2011-08-22T22:16:17Z', 
-# u'url': u'https://github.com/gimenete/iOS-boilerplate', 
-# u'type': u'repo', u'created_at': u'2011-08-22T22:16:17Z', 
-# u'pushed_at': u'2012-02-28T15:44:29Z', 
-# u'open_issues': 16}
-
-def format_repo(repo):
+def format_repo_to_screen(repo):
     json = repo.to_json()
     return "Username: %s\n  %s [Fork:%s]\n  %s  %s\n  %s  %s  %s" % (json['owner'], 
         json['name'], json['fork'], json['description'], json['url'], 
         json['homepage'], json['followers'], json['forks'])
 
-for repo in lib.retrieve_repos_by_keyword(keywords, language):
-    print format_repo(repo)
+repos = lib.retrieve_repos_by_keyword(keywords, language)
+
+
+
+if options.filename:
+    writer = csv.writer(f, encoding='utf-8', delimiter='\t')
+    print "Processing. Please wait..."
+    writer.writerow(("#", "owner", "name", "fork", 
+            "description", "url", "homepage", "followers", "forks",))
+    i = 0
+    for repo in repos:
+        i += 1
+        json = repo.to_json()
+        writer.writerow((i, json["owner"], json["name"], '1' if json["fork"] else '0', 
+            json["description"], json["url"], json["homepage"], json["followers"], json["forks"],))
+    print "\nTotal records count: %s" % i
+    print "Thank you."
+
+else:
+    for repo in repos:
+        print format_repo_to_screen(repo)
