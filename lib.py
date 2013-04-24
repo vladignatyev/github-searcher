a69893b9c3b60911932458fe4990124ab4fd2648
Vladimir Ignatyev <vignatyev@flexis.ru>
2013-04-24 10:51:59 +0400

    output changed to html, added github auth to improve quota

--- lib.py
+++ lib.py
@@ -1,16 +1,31 @@
-from github3 import GitHub
+from github3 import GitHub, login
 
 OBJECTIVE_C = 'Objective-C'
 
-def retrieve_repos_by_keyword(keywords, language):
-    gh = GitHub()
+def retrieve_repos_by_keyword(keywords, language, username='', password=''):
+    gh = None
+    if username and password:
+        gh = login(username, password=password)
+    else:
+        gh = GitHub()
     try:
         for keyword in keywords:
             for page in range(1, 11):
                 repos = gh.search_repos(keyword, language=language, start_page=page)
                 for repo in repos:
-                    if repo.to_json()['language'] == language:
-                        yield repo
-    except Exception:
+                    r = repo.to_json()
+                    if r['language'] == language:
+                        result = r
+                        try:
+                            user = gh.user(r['owner']).to_json()
+                            result['owner_email'] = user.get('email', '')
+                            result['owner_blog'] = user.get('blog', '')
+                            result['owner_location'] = user.get('location', '')
+                            result['owner_avatar'] = user.get('avatar_url', '')
+                        except Exception as e:
+                            pass
+                        yield result
+    except Exception as e:
+        print e
         pass
 
--- retrieve.py
+++ retrieve.py
@@ -16,9 +16,11 @@ if options.filename:
     print "Writing report to file: %s" % options.filename
     print "---"
 
+username = raw_input('Username on GitHub [Enter to skip]:')
+password = raw_input('Password on GitHub [Enter to skip]:')
+
 keywords = raw_input('Enter keywords, divided with comma:').split(',')
 language = raw_input('Enter language [Objective-C]:')
-
 if not language:
     language = lib.OBJECTIVE_C
 
@@ -28,24 +30,70 @@ def format_repo_to_screen(repo):
         json['name'], json['fork'], json['description'], json['url'], 
         json['homepage'], json['followers'], json['forks'])
 
-repos = lib.retrieve_repos_by_keyword(keywords, language)
+def generate_file_report(model, file):
+    f.write('<html><table border="1">')
+    fields = ["#", "owner", "owner_avatar", "owner_email", "owner_blog", "owner_location", 
+    "name", "url", "homepage", "description", "followers", "forks"]
 
+    f.write('<tr>')
+    for field in fields:
+        field_layout = field
+        f.write(u'<td>%s</td>' % field_layout)
+    f.write(u'</tr>')
 
+    i = 0 
+    for repo in model:
+        i += 1
+        f.write(u'<tr>')
+        for field in fields:
+            if field not in ['forks', 'followers', '#']:
+                field_value = repo.get(field, '')
+                if field_value is None:
+                    value = ''
+                else:
+                    value = field_value.encode('utf-8')
+            else:
+                value = str(repo.get(field, ''))
+            if field == '#':
+                value = str(i)
+                field_layout = i
+
+            if field == 'owner_email':
+                field_layout = u'<a href="mailto:%s">%s</a>' % (value, value)
+            elif field == 'owner_blog' or field == 'url' or field == 'homepage':
+                field_layout = u'<a href="%s">%s</a>' % (value, value)
+            elif field == 'owner_avatar':
+                field_layout = u'<img src="%s" width="128" />' % value
+            else:
+                field_layout = value
+
+            f.write('<td>')
+            f.write(field_layout)
+            f.write('</td>')
+        f.write(u'</tr>')
+    f.write(u'</table></html>')
+    f.close()
+
+def generate_screen_report(model):
+    for repo in model:
+        print format_repo_to_screen(repo)
+
+repos = lib.retrieve_repos_by_keyword(keywords, language, username, password)
+
+repos_model = []
+
+owners_emails_and_blogs = {}
+
+print "Processing. Please wait..."
+i = 0
+for repo in repos:
+    i += 1
+    repos_model.append(repo)
 
 if options.filename:
-    writer = csv.writer(f, encoding='utf-8', delimiter='\t')
-    print "Processing. Please wait..."
-    writer.writerow(("#", "owner", "name", "fork", 
-            "description", "url", "homepage", "followers", "forks",))
-    i = 0
-    for repo in repos:
-        i += 1
-        json = repo.to_json()
-        writer.writerow((i, json["owner"], json["name"], '1' if json["fork"] else '0', 
-            json["description"], json["url"], json["homepage"], json["followers"], json["forks"],))
-    print "\nTotal records count: %s" % i
-    print "Thank you."
-
+    generate_file_report(repos_model, f)
 else:
-    for repo in repos:
-        print format_repo_to_screen(repo)
+    generate_screen_report(repos_model)
+
+print "\nTotal records count: %s" % i
+print "Thank you."
